05d67bd27561e28f96e08788d66c42bf005ff027
Willian Antunes <willian.lima.antunes@gmail.com>
2018-09-15 16:41:40 -0300

    Start of Vue.js In Action and all the code used for chapter 2

---
+++ Vue.js in Action/Chapter 2/life-cycle-hooks.js
@@ -0,0 +1,50 @@
+// https://vuejs.org/v2/api/#Instance-Methods-Lifecycle
+
+var APP_LOG_LIFECYCLE_EVENTS = true;       //
+
+var webstore = new Vue({
+    el: "#app",
+    data: {
+        sitename: "Vue.js Pet Depot",
+    },
+    beforeCreate: function() {      //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("beforeCreate"); //
+        }  //
+    },   //
+    created: function() {   //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("created");     //
+        }  //
+    },   //
+    beforeMount: function() {       //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("beforeMount"); //
+        }  //
+    },   //
+    mounted:  function() {  //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("mounted");     //
+        } //
+    },   //
+    beforeUpdate:  function() {     //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("beforeUpdate");         //
+        } //
+    },   //
+    updated:  function() { //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("updated");     //
+        } //
+    },   //
+    beforeDestroy:  function() {    //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("beforeDestroy ");       //
+        } //
+    },   //
+    destroyed:  function() {        //
+        if (APP_LOG_LIFECYCLE_EVENTS) {        //
+            console.log("destroyed");   //
+        } //
+    }    //
+});
---
+++ Vue.js in Action/README.md
@@ -0,0 +1,7 @@
+# Vue.js In Action
+
+Coded lessons from the book that you can find [here](https://www.manning.com/books/vue-js-in-action).
+
+All the code is available also at the following: 
+
+- https://github.com/ErikCH/VuejsInActionCode
