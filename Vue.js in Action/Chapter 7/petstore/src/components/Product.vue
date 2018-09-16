0f8bd72c8e69a2f536b70f49649ea0f1ab9a04f7
Willian Antunes <willian.lima.antunes@gmail.com>
2018-09-16 10:06:39 -0300

    Minimum work to make http://localhost:8080/product/1001 works

---
+++ Vue.js in Action/Chapter 7/petstore/src/components/Product.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <my-header></my-header>
+
+    <!-- REQUEST PATH PAREMETER IS HERE! -->
+    <h1> This is the id {{ $route.params.id}}</h1>
+
+    <div class="row">
+
+      <div class="col-md-5 col-md-offset-0">
+        <figure>
+          <img class="product" v-bind:src="product.image">
+        </figure>
+      </div>
+
+      <div class="col-md-6 col-md-offset-0 description">
+        <h1>{{product.title}}</h1>
+        <p v-html="product.description"></p>
+        <p class="price">{{product.price }}</p>
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+  import MyHeader from './Header.vue'
+
+  export default {
+    components: {MyHeader},
+    data() {
+      return {
+        product: ''
+      }
+    },
+    created: function () {
+      axios.get('/static/products.json')
+        .then((response) => {
+          this.product = response.data.products.filter(data => data.id == this.$route.params.id)[0]
+          this.product.image = '/' + this.product.image;
+        });
+    }
+  }
+</script>
--- Vue.js in Action/Chapter 7/petstore/src/router/index.js
+++ Vue.js in Action/Chapter 7/petstore/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Form from '@/components/Form'
 import Main from '@/components/Main'
+import Product from '@/components/Product'
 
 Vue.use(Router);
 
@@ -21,6 +22,12 @@ export default new Router({
       path: '/form',
       name: 'Form',
       component: Form,
+      props: true
+    },
+    {
+      path: '/product/:id',
+      name: 'Id',
+      component: Product,
       props: true
     }
 
