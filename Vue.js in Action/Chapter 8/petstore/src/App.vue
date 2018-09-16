4a5f1d118b691b7c9773f52d92a2ac4fd528101f
Willian Antunes <willian.lima.antunes@gmail.com>
2018-09-16 15:22:52 -0300

    Transition between each route

--- Vue.js in Action/Chapter 8/petstore/src/App.vue
+++ Vue.js in Action/Chapter 8/petstore/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,6 +13,11 @@
 </script>
 
 <style>
-  #app {
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease-out;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
   }
 </style>
